891b08f3f5843112b653c07351d39160e10e552d
AntonZadereiko <wieze@ukr.net>
2023-07-13 12:29:22 +0300

    add hw3

---
+++ scripts.js
@@ -0,0 +1,4 @@
+let entrance = confirm('Ви перейшли на основну сторінку.');
+let name = prompt('Як вас звати?');
+let age = prompt('Скільки вам років?');
+alert('Вітаю ' + name + ' ! \nВаш вік '  + age + ' !' );
