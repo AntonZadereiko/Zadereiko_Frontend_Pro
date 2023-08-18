67e72dad676c4286dcf9182ea1800ed9730b7fff
AntonZadereiko <wieze@ukr.net>
2023-08-18 22:22:15 +0300

    homework 3

--- scripts.js
+++ scripts.js
@@ -8,8 +8,8 @@ alert('Вітаю ' + name + ' ! \nВаш вік '  + age + ' !' );
 // 2 task
 
 const number = window.prompt("Введіть 5-значне число :");
-if (number.length>=6) {
-    alert("Упс, забагато символів. Спробуйте знову")
+if (number.length !==5) {
+    alert("Шкода, але ви ввели не 5-значне число. Спробуйте знову")
 } else {
     let numbers = number.split("").join(" ");
     alert("Ваше число : " + numbers)
