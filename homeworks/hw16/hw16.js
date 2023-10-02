1f6f26c2e4856f13f55cc940bb66e8b8512dea66
AntonZadereiko <wieze@ukr.net>
2023-10-02 20:59:22 +0300

    hw 16 is done.

---
+++ homeworks/hw16/hw16.js
@@ -0,0 +1,29 @@
+const Roles = Object.freeze({
+    Admin: 'admin',
+    Editor: 'editor',
+    Reader: 'reader',
+    SuperAdmin: 'superAdmin'
+})
+
+class User {
+    constructor(id, firstName, lastName, role) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        Object.defineProperty(this, 'role', {
+            value: role,
+            writable: false,
+            configurable: false,
+        });
+    }
+}
+
+console.log(Roles.Admin);
+Roles.Admin = 'hello';
+Roles.newCondition = 'value';
+
+const editor = new User(1, 'Randy', 'Orton', Roles.Admin);
+console.log(editor.role);
+console.log(editor.role === Roles.Admin);
+editor.role = false;
+delete editor.role;
