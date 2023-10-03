80578076e58ab1246b193aa5df8e7f143af251b0
AntonZadereiko <wieze@ukr.net>
2023-10-03 16:50:46 +0300

    hw 9 upgraded

--- homeworks/hw9/hw9.js
+++ homeworks/hw9/hw9.js
@@ -1,120 +1,78 @@
+
+
 // 1 Знайти суму та кількість позитивних елементів.
-// let arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-// let sum = 0;
-// let evenNum = 0;
-//
-// for (let element of arr) {
-//     if (element > 0) {
-//         sum += element;
-//         evenNum++;
-//     }
-// }
-//
-// console.log("Сума позитивних елементів: " + sum);
-// console.log("Кількість позитивних елементів: " + evenNum);
+let arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+const isPositive = (n) => n > 0;
+const isEven = (n) => n % 2 === 0;
+const sumReducer = (acc, n) => acc + n;
+const posNums = arr.filter(isPositive);
+const posSum = posNums.reduce(sumReducer);
+
+const positiveNumbers = arr.filter((n) => n > 0);
+const sum = positiveNumbers.reduce((sum, n) => sum + n, 0);
+const result = arr.filter((n) => n > 0)
+    .reduce((acc, n) => {
+        acc.sum += n;
+        acc.count += 1;
+        return acc;
+    }, {sum: 0, count: 0});
+
+console.log("Сума позитивних елементів: ", result.sum);
+console.log("Кількість позитивних елементів: ", result.count);
+
 
 // 2 Знайти мінімальний елемент масиву та його порядковий номер.
-// let arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-// let minElement = arr[0];
-// let ordinalNumber = 0;
-// for ( let i = 0; i < arr.length; i++){
-//     if (arr[i] < minElement) {
-//         minElement = arr[i];
-//         ordinalNumber = i;
-//     }
-// }
-// console.log("Мінімальний елемент: " + minElement);
-// console.log("Порядковий номер мінімального елемента: " + ordinalNumber);
+const min = Math.min.apply(null, arr);
+const indexOfMinElement = arr.indexOf(min);
+
+console.log("Мінімальний елемент: " + min);
+console.log("Порядковий номер мінімального елемента: " + indexOfMinElement);
+
 
 // 3 Знайти максимальний елемент масиву та його порядковий номер.
-// let arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-// let maxElement = arr[0];
-// let ordinalNumber = 0;
-// for ( let i = 0; i < arr.length; i++){
-//     if (arr[i] > maxElement) {
-//         maxElement = arr[i];
-//         ordinalNumber = i;
-//     }
-// }
-// console.log("Максимальний елемент: " + maxElement);
-// console.log("Порядковий номер максимального елемента: " + ordinalNumber);
+const max = Math.max.apply(null, arr);
+const indexOfMaxElement = arr.indexOf(max);
+console.log("Максимальний елемент: " + max);
+console.log("Порядковий номер максимального елемента: " + indexOfMaxElement);
+
 
 // 4 Визначити кількість негативних елементів.
-// let arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-// let negativeNum = 0;
-// for ( let element of arr){
-//     if ( element < 0) {
-//         negativeNum++;
-//     }
-// }
-// console.log( 'Кількість негативних елементів : ' + negativeNum);
+
+
+const negativeElsNumber = arr.filter((n) => n < 0).length;
+console.log( 'Кількість негативних елементів : ' + negativeElsNumber);
+
 
 // 5 Знайти кількість непарних позитивних елементів.
-// let arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-// let positiveNumber = 0;
-//
-// for ( let element of arr) {
-//     if ( element > 0 && element % 2 !== 0){
-//         positiveNumber++;
-//     }
-// }
-// console.log('Кількість непарних позитивних елементів : ' + positiveNumber + ';');
+
+const countPositive = posNums.filter((n) =>!isEven(n)).length;
+console.log('Кількість непарних позитивних елементів : ' + countPositive);
+
 
 // 6 Знайти кількість парних позитивних елементів.
-// let arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-// let evenNumber = 0;
-//
-// for ( let element of arr) {
-//     if ( element > 0 && element % 2 === 0){
-//         evenNumber++;
-//     }
-// }
-// console.log('Кількість парних позитивних елементів : ' + evenNumber + ';');
+
+const posEvenNums = posNums.filter((isEven));
+const posEvenNumsLength = posEvenNums.length;
+console.log('Кількість парних позитивних елементів : ' + posEvenNumsLength);
 
 // 7 Знайти суму парних позитивних елементів.
-// let arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-// let sumEven = 0;
-// for ( let element of arr) {
-//     if ( element > 0 && element % 2 === 0) {
-//         sumEven += element;
-//     }
-// }
-// console.log("Сума парних позитивних елементів: " + sumEven);
+
+const sumPosEven = posEvenNums.reduce(sumReducer);
+console.log("Сума парних позитивних елементів: " + sumPosEven);
 
 // 8 Знайти суму непарних позитивних елементів.
-// let arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-// let sumOdd = 0;
-// for ( let element of arr) {
-//     if ( element > 0 && element % 2 !== 0) {
-//         sumOdd += element;
-//     }
-// }
-// console.log("Сума непарних позитивних елементів: " + sumOdd);
+
+const sumNegOdd = arr.filter(n => !isPositive(n) && !isEven(n)).reduce(sumReducer);
+console.log("Сума непарних позитивних елементів: " + sumNegOdd);
 
 // 9 Знайти добуток позитивних елементів.
-// let arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-// let divPositive = 1;
-// for ( let element of arr) {
-//     if ( element > 0) {
-//         divPositive *= element;
-//     }
-// }
-// console.log( 'Добуток позитивних елементів : ' + divPositive);
+
+const posProduct = posNums.reduce((product, n) => product * n);
+console.log( 'Добуток позитивних елементів : ' + posProduct);
 
 // 10 Знайти найбільший серед елементів масиву, остальні обнулити.
-let arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-let maxElement = arr[0];
-for (let element of arr) {
-    if (element > maxElement) {
-        maxElement = element;
-    }
-}
-console.log('Найбільший серед елементів масиву : ' + maxElement);
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== maxElement) {
-        arr[i] = 0;
-    }
-}
-console.log("Масив після обнулення : " + arr);
+
+const maxAndDelete = arr.map((n) => (n === max ? n : 0 ));
+console.log('Найбільший серед елементів масиву і інші обнулив : ',maxAndDelete);
 
 
