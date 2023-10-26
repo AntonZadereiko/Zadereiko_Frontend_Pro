9145f5de36361f1acde4426099fc10c5aa4b2399
AntonZadereiko <wieze@ukr.net>
2023-10-26 16:08:55 +0300

    hw 24 is done

--- homeworks/hw24/hw24.js
+++ homeworks/hw24/hw24.js
@@ -1,18 +1,23 @@
 function toCamelCase(input) {
-    const matches = input.match(/_([a-z])/g);
-
-    if (!matches) {
-        return input;
-    }
-
-    for (const match of matches) {
-        input = input.replace(match, match.charAt(1).toUpperCase());
-    }
-
-    return input;
+    return input.replace(/_([a-z])/g, (match, group) => group.toUpperCase() );
 }
 
-console.log(toCamelCase('hello_world'));
+console.log(toCamelCase('hello_world_its_me'));
 console.log(toCamelCase('very_long_property_name'));
 console.log(toCamelCase('kebab-case-key'));
 
+const response = {
+    user_id: 'abc123',
+    company_name: 'Hillel',
+    contract_expiration_date: '22/11/2013',
+    'secret-token' : 'unique$ecret',
+};
+
+let camelCasedResponse = {};
+
+for (let key in response) {
+    const newKey = toCamelCase(key);
+    camelCasedResponse[newKey] = response[key];
+}
+
+console.log(camelCasedResponse);
