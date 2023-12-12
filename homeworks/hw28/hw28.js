fc89c04574cfb4441150aaa48889e56627e4e0c2
AntonZadereiko <wieze@ukr.net>
2023-12-12 13:08:31 +0200

    added fetch

--- homeworks/hw28/hw28.js
+++ homeworks/hw28/hw28.js
@@ -1,27 +1,47 @@
-function getWeather() {
-    const xhr = new XMLHttpRequest();
-    const city = "LVIV";
-    const apiKey = "5d066958a60d315387d9492393935c19";
-   // const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
-   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
-    xhr.open("GET", url, true);
+// function getWeather() {
+//     const xhr = new XMLHttpRequest();
+//     const city = "LVIV";
+//     const apiKey = "5d066958a60d315387d9492393935c19";
+//    // const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
+//    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
+//     xhr.open("GET", url, true);
 
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            var data = JSON.parse(xhr.responseText);
+//     xhr.onreadystatechange = function () {
+//         if (xhr.readyState == 4 && xhr.status == 200) {
+//             var data = JSON.parse(xhr.responseText);
 
     
-            document.getElementById("temperature").innerHTML = `${data.main.temp}°C`;
-            document.getElementById("pressure").innerHTML = `${data.main.pressure} hPa`;
-            document.getElementById("description").innerHTML = `${data.weather[0].description}`;
-            document.getElementById("humidity").innerHTML = `${data.main.humidity}%`;
-            document.getElementById("wind-speed").innerHTML = `${data.wind.speed} м/с`;
-            document.getElementById("wind-direction").innerHTML = `${data.wind.deg}°`;
+//             document.getElementById("temperature").innerHTML = `${data.main.temp}°C`;
+//             document.getElementById("pressure").innerHTML = `${data.main.pressure} hPa`;
+//             document.getElementById("description").innerHTML = `${data.weather[0].description}`;
+//             document.getElementById("humidity").innerHTML = `${data.main.humidity}%`;
+//             document.getElementById("wind-speed").innerHTML = `${data.wind.speed} м/с`;
+//             document.getElementById("wind-direction").innerHTML = `${data.wind.deg}°`;
+//             document.getElementById("weather-icon").src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+//         }
+//     };
+// // why problem in xhr.send() ?
+//     xhr.send();
+// }
+
+// getWeather();
+function getWeather() {
+    const city = "LVIV";
+    const apiKey = "5d066958a60d315387d9492393935c19";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("temperature").innerHTML = `Температура: ${data.main.temp}°C`;
+            document.getElementById("pressure").innerHTML = `Тиск: ${data.main.pressure} hPa`;
+            document.getElementById("description").innerHTML = `Опис: ${data.weather[0].description}`;
+            document.getElementById("humidity").innerHTML = `Вологість: ${data.main.humidity}%`;
+            document.getElementById("wind-speed").innerHTML = `Швидкість вітру: ${data.wind.speed} м/с`;
+            document.getElementById("wind-direction").innerHTML = `Напрям вітру: ${data.wind.deg}°`;
             document.getElementById("weather-icon").src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-        }
-    };
-// why problem in xhr.send() ?
-    xhr.send();
+        })
+        .catch(error => console.error('Помилка при отриманні погоди:', error));
 }
 
 getWeather();
