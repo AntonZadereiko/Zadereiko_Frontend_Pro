00d419aaf19043ae8de4de5908b4709a72370e83
AntonZadereiko <wieze@ukr.net>
2023-12-17 16:05:32 +0200

    almost done hw29

--- my-app/my-app-name/src/App.js
+++ my-app/my-app-name/src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/Form/Form";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [allTodos, setAllTodos] = useState(0);
+  const [allComplete, setAllComplete] = useState(0);
+  const [showCompleted, setShowCompleted] = useState(false);
+
+  const putTodo = (value) => {
+    if (value) {
+      setTodos([...todos, { id: Date.now(), text: value, done: false }]);
+      setAllTodos(allTodos + 1);
+    } else {
+      alert("Type text");
+    }
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id !== id) return todo;
+
+        const updatedTodo = {
+          ...todo,
+          done: !todo.done,
+        };
+
+        if (updatedTodo.done) {
+          setAllComplete(allComplete + 1);
+        } else {
+          setAllComplete(allComplete - 1);
+        }
+
+        return updatedTodo;
+      })
+    );
+  };
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+    setAllTodos(allTodos - 1);
+    const deletedTodo = todos.find((todo) => todo.id === id);
+    if (deletedTodo && deletedTodo.done) {
+      setAllComplete(allComplete - 1);
+    }
+  };
+
+  const clearTodos = () => {
+    setTodos([]);
+    setAllTodos(0);
+    setAllComplete(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="container">
+        <h1 className="title">TodoList</h1>
+        <Form putTodo={putTodo} />
+        <ul className="todos">
+          {todos
+            .filter((todo) => (showCompleted ? todo.done : !todo.done))
+            .map((todo) => (
+              <li
+                className={todo.done ? "todo done" : "todo"}
+                key={todo.id}
+                onClick={() => toggleTodo(todo.id)}
+              >
+                {todo.text}
+                <img
+                  src="./remove.png"
+                  alt="delete"
+                  className="delete"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    removeTodo(todo.id);
+                  }}
+                />
+              </li>
+            ))}
+          <div className="info">
+            <span>All todos: {allTodos}</span>
+            <span>Complete: {allComplete}</span>
+          </div>
+          <button className="btn" onClick={clearTodos}>
+            Clear all
+          </button>
+          <button
+            className="btn"
+            onClick={() => setShowCompleted(!showCompleted)}
+          >
+            {showCompleted ? "Hide completed" : "Show completed"}
+          </button>
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
